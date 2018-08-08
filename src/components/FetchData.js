8d207c0d49db54c35a9faee9d0a370762475d7ed
Kajsa Stoll Kuroczik <25846144+kajsastollk@users.noreply.github.com>
2018-08-08 14:13:28 +0200

    first commit

---
+++ src/components/FetchData.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class FetchData extends Component {
+  state = {
+    trendingMovies: []
+  }
+
+
+  //Life cycle method
+  componentDidMount() {
+    this.fetchDataFromAPI();
+  }
+
+  fetchDataFromAPI = () => {
+    fetch('https://javascriptst18.herokuapp.com/trending?_limit=10')
+      .then(response => response.json())
+      .then((trendingMovies) => {
+        this.setState({ trendingMovies: trendingMovies });
+      });
+  }
+
+  //render MÅSTE finnas
+  render() {
+    const movies = this.state.trendingMovies.map((movie) => {
+      return (
+        <div>
+          <p> {movie.title} </p>
+          <em> {movie.rating} </em>
+        </div>
+      );
+    });
+
+    if (this.state.trendingMovies.length) {
+      return (
+        <div>
+          {movies}
+        </div >
+      );
+    } else {
+      return (
+        <p> LOADING! </p>
+      );
+    }
+
+  }
+}
+
+export default FetchData;
